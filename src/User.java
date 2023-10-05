e96fa407f5764d36953b8bc0c690795bc84780c7
Jan Kowalski <jan.kowalski@pollub.edu.pl>
2023-10-05 15:29:37 +0200

    Feat/pollub-2

---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+
+    public void addUser() {
+        System.out.println("Adduser");
+    }
+
+    public void deleteUser() {
+        System.out.println("deleteUser");
+    }
+
+}
